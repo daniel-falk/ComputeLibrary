27e9e4a27dfa5bed43b1405d450f989604f1883e
Daniel Falk <daniel@da-robotteknik.se>
2018-01-14 15:52:32 +0100

    Rename x_u8 to x_qs8

--- examples/neon_convolution_benchmark.cpp
+++ examples/neon_convolution_benchmark.cpp
@@ -19,24 +19,24 @@ const uint8_t kernel4x4[] = {
 
 // ****** Unsigned 8 (fixpoint) conv ********
 
-NEConvolutionLayer conv_u8;
-Tensor             w_u8;
-Tensor             b_u8;
+NEConvolutionLayer conv_qs8;
+Tensor             w_qs8;
+Tensor             b_qs8;
 
-void acl_init_u8(Tensor *src, Tensor *dst) {
+void acl_init_qs8(Tensor *src, Tensor *dst) {
     const TensorShape w_shape(static_cast<unsigned int>(4), static_cast<unsigned int>(4));
     const TensorShape b_shape(static_cast<unsigned int>(1));
-    w_u8.allocator()->init(TensorInfo(w_shape, 1, DataType::QS8, 5));
-    b_u8.allocator()->init(TensorInfo(b_shape, 1, DataType::QS8, 5));
-    w_u8.allocator()->allocate();
-    b_u8.allocator()->allocate();
-    memcpy((void *)(w_u8.buffer()), (void *)(kernel4x4), 16);
+    w_qs8.allocator()->init(TensorInfo(w_shape, 1, DataType::QS8, 5));
+    b_qs8.allocator()->init(TensorInfo(b_shape, 1, DataType::QS8, 5));
+    w_qs8.allocator()->allocate();
+    b_qs8.allocator()->allocate();
+    memcpy((void *)(w_qs8.buffer()), (void *)(kernel4x4), 16);
 
-    conv_u8.configure(src, &w_u8, &b_u8, dst, PadStrideInfo(1,1,0,0));
+    conv_qs8.configure(src, &w_qs8, &b_qs8, dst, PadStrideInfo(1,1,0,0));
 }
 
-void acl_u8() {
-    conv_u8.run();
+void acl_qs8() {
+    conv_qs8.run();
 }
 
 // ****** Floating Point 32 conv ******* 
@@ -74,34 +74,34 @@ int main(int argc, const char **argv) {
     ARM_COMPUTE_UNUSED(argv);
 
     // Setup source images
-    Tensor src_u8;
-    Tensor dst_u8;
+    Tensor src_qs8;
+    Tensor dst_qs8;
     Tensor src_f32;
     Tensor dst_f32;
 
     const TensorShape src_shape(static_cast<unsigned int>(640), static_cast<unsigned int>(480));
     const TensorShape dst_shape(static_cast<unsigned int>(637), static_cast<unsigned int>(477));
 
-    src_u8.allocator()->init(TensorInfo(src_shape, 1, DataType::QS8, 5));
-    dst_u8.allocator()->init(TensorInfo(dst_shape, 1, DataType::QS8, 5));
+    src_qs8.allocator()->init(TensorInfo(src_shape, 1, DataType::QS8, 5));
+    dst_qs8.allocator()->init(TensorInfo(dst_shape, 1, DataType::QS8, 5));
     src_f32.allocator()->init(TensorInfo(src_shape, 1, DataType::F32));
     dst_f32.allocator()->init(TensorInfo(dst_shape, 1, DataType::F32));
 
-    src_u8.info()->auto_padding();
+    src_qs8.info()->auto_padding();
     src_f32.info()->auto_padding();
 
-    src_u8.allocator()->allocate();
-    dst_u8.allocator()->allocate();
+    src_qs8.allocator()->allocate();
+    dst_qs8.allocator()->allocate();
     src_f32.allocator()->allocate();
     dst_f32.allocator()->allocate();
 
-    acl_init_u8(&src_u8, &dst_u8);
+    acl_init_qs8(&src_qs8, &dst_qs8);
     acl_init_f32(&src_f32, &dst_f32);
 
     /// Fill input vector with data
     {
-        uint8_t *ptr = reinterpret_cast<uint8_t *>(src_u8.buffer());
-        for (size_t i = 0; i < src_u8.info()->total_size(); i++) {
+        uint8_t *ptr = reinterpret_cast<uint8_t *>(src_qs8.buffer());
+        for (size_t i = 0; i < src_qs8.info()->total_size(); i++) {
             *(ptr++) = static_cast<uint8_t>(i % 256);
         }
     }
@@ -116,13 +116,13 @@ int main(int argc, const char **argv) {
     const std::string npy1 = "f32_src.npy";
     save_to_npy(src_f32, npy1, false);
 
-    // Time u8
+    // Time qs8
     clock_t begin = clock();
     for (int i = 0; i < 10; i++)
-        acl_u8();
+        acl_qs8();
     clock_t end = clock();
     double t = double(end - begin) / CLOCKS_PER_SEC;
-    printf("U8: Elapsed time %.2f S, avg: %.2f mS\n", t, t * 100);
+    printf("QS8: Elapsed time %.2f S, avg: %.2f mS\n", t, t * 100);
 
     // Time f32
     begin = clock();
