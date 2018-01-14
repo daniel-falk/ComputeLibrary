5e68745024769f3b812d7548d43a5fd998d5bc4b
Daniel Falk <daniel@da-robotteknik.se>
2018-01-14 17:55:07 +0100

    Add U8 convolution and enable auto-padding
    - Added U8 convolution (with manual bias)
    - Enabled auto-padding and use correct buffer offsets

--- examples/neon_convolution_benchmark.cpp
+++ examples/neon_convolution_benchmark.cpp
@@ -10,13 +10,28 @@ using namespace arm_compute;
 using namespace utils;
 
 // 4x4 kernel
-const uint8_t kernel4x4[] = {
-    54, 114, 154, 56,
-    65, 15,  2,   26,
-    13, 244, 133, 55,
-    98, 12,  98,  199
+const int16_t kernel3x3[] = {
+    0, 0, 1,
+    0, 1, 0,
+    0, 0, 1
 };
+const uint8_t bias = 98;
 
+// ****** Unsigned 8 conv (kernel-pass only) ********
+
+NEConvolution3x3 conv_u8;
+
+void acl_init_u8(Tensor *src, Tensor *dst) {
+    conv_u8.configure(src, dst, kernel3x3, 1, BorderMode::UNDEFINED);
+}
+
+void acl_u8(Tensor *dst) {
+    conv_u8.run();
+    uint8_t *ptr = dst->buffer();
+    for (size_t i = 0; i < dst->info()->total_size(); i++) {
+        ptr[i] = bias > 0xFF - ptr[i] ? 0xFF : ptr[i] + bias;
+    }
+}
 // ****** Unsigned 8 (fixpoint) conv ********
 
 NEConvolutionLayer conv_qs8;
@@ -24,13 +39,19 @@ Tensor             w_qs8;
 Tensor             b_qs8;
 
 void acl_init_qs8(Tensor *src, Tensor *dst) {
-    const TensorShape w_shape(static_cast<unsigned int>(4), static_cast<unsigned int>(4));
+    const TensorShape w_shape(static_cast<unsigned int>(3), static_cast<unsigned int>(3));
     const TensorShape b_shape(static_cast<unsigned int>(1));
     w_qs8.allocator()->init(TensorInfo(w_shape, 1, DataType::QS8, 5));
     b_qs8.allocator()->init(TensorInfo(b_shape, 1, DataType::QS8, 5));
     w_qs8.allocator()->allocate();
     b_qs8.allocator()->allocate();
-    memcpy((void *)(w_qs8.buffer()), (void *)(kernel4x4), 16);
+
+    uint8_t *ptr = w_qs8.buffer();
+    for (size_t i = 0; i < 9; i++) {
+        *(ptr++) = static_cast<uint8_t>(kernel3x3[i]);
+    }
+
+    *(b_qs8.buffer()) = bias;
 
     conv_qs8.configure(src, &w_qs8, &b_qs8, dst, PadStrideInfo(1,1,0,0));
 }
@@ -46,7 +67,7 @@ Tensor             w_f32;
 Tensor             b_f32;
 
 void acl_init_f32(Tensor *src, Tensor *dst) {
-    const TensorShape w_shape(static_cast<unsigned int>(4), static_cast<unsigned int>(4));
+    const TensorShape w_shape(static_cast<unsigned int>(3), static_cast<unsigned int>(3));
     const TensorShape b_shape(static_cast<unsigned int>(1));
     w_f32.allocator()->init(TensorInfo(w_shape, 1, DataType::F32));
     b_f32.allocator()->init(TensorInfo(b_shape, 1, DataType::F32));
@@ -54,11 +75,11 @@ void acl_init_f32(Tensor *src, Tensor *dst) {
     b_f32.allocator()->allocate();
 
     float *ptr = reinterpret_cast<float *>(w_f32.buffer());
-    for (size_t i = 0; i < 16; i++) {
-        *(ptr++) = static_cast<float>(kernel4x4[i]);
+    for (size_t i = 0; i < 9; i++) {
+        *(ptr++) = static_cast<float>(kernel3x3[i]);
     }
 
-    *(reinterpret_cast<float *>(b_f32.buffer())) = 4.;
+    *(reinterpret_cast<float *>(b_f32.buffer())) = static_cast<float>(bias);
 
     conv_f32.configure(src, &w_f32, &b_f32, dst, PadStrideInfo(1,1,0,0));
 }
@@ -74,54 +95,103 @@ int main(int argc, const char **argv) {
     ARM_COMPUTE_UNUSED(argv);
 
     // Setup source images
+    Tensor src_u8;
+    Tensor dst_u8;
     Tensor src_qs8;
     Tensor dst_qs8;
     Tensor src_f32;
     Tensor dst_f32;
 
     const TensorShape src_shape(static_cast<unsigned int>(640), static_cast<unsigned int>(480));
-    const TensorShape dst_shape(static_cast<unsigned int>(637), static_cast<unsigned int>(477));
+    const TensorShape dst_shape(static_cast<unsigned int>(638), static_cast<unsigned int>(478));
 
-    src_qs8.allocator()->init(TensorInfo(src_shape, 1, DataType::QS8, 5));
-    dst_qs8.allocator()->init(TensorInfo(dst_shape, 1, DataType::QS8, 5));
+    src_u8.allocator()->init(TensorInfo(src_shape.x(), src_shape.y(), Format::U8));
+    dst_u8.allocator()->init(TensorInfo(src_shape.x(), src_shape.y(), Format::U8));
+    src_qs8.allocator()->init(TensorInfo(src_shape, 1, DataType::QS8, 0));
+    dst_qs8.allocator()->init(TensorInfo(dst_shape, 1, DataType::QS8, 0));
     src_f32.allocator()->init(TensorInfo(src_shape, 1, DataType::F32));
     dst_f32.allocator()->init(TensorInfo(dst_shape, 1, DataType::F32));
 
+    src_u8.info()->auto_padding();
     src_qs8.info()->auto_padding();
     src_f32.info()->auto_padding();
 
+    acl_init_u8(&src_u8, &dst_u8);
+    acl_init_qs8(&src_qs8, &dst_qs8);
+    acl_init_f32(&src_f32, &dst_f32);
+
+    src_u8.allocator()->allocate();
+    dst_u8.allocator()->allocate();
     src_qs8.allocator()->allocate();
     dst_qs8.allocator()->allocate();
     src_f32.allocator()->allocate();
     dst_f32.allocator()->allocate();
 
-    acl_init_qs8(&src_qs8, &dst_qs8);
-    acl_init_f32(&src_f32, &dst_f32);
-
     /// Fill input vector with data
     {
-        uint8_t *ptr = reinterpret_cast<uint8_t *>(src_qs8.buffer());
-        for (size_t i = 0; i < src_qs8.info()->total_size(); i++) {
-            *(ptr++) = static_cast<uint8_t>(i % 256);
+        uint8_t *ptr = src_u8.buffer();
+        int x, y;
+        for (x = y = 0; true; x++) {
+            if (static_cast<unsigned int>(x) == src_shape.x()) {
+                y++;
+                x = 0;
+                if (src_shape.y() == static_cast<unsigned int>(y)) {
+                    break;
+                }
+            }
+            ptr[src_u8.info()->offset_element_in_bytes(Coordinates(x, y))] = static_cast<uint8_t>((y * src_shape.x() + x) % 256);
         }
     }
     {
-        float *ptr = reinterpret_cast<float *>(src_f32.buffer());
-        for (size_t i = 0; i < src_f32.info()->total_size() / sizeof(float); i++) {
-            *(ptr++) = static_cast<float>(i % 256);
+        uint8_t *ptr = src_qs8.buffer();
+        int x, y;
+        for (x = y = 0; true; x++) {
+            if (static_cast<unsigned int>(x) == src_shape.x()) {
+                y++;
+                x = 0;
+                if (src_shape.y() == static_cast<unsigned int>(y)) {
+                    break;
+                }
+            }
+            ptr[src_qs8.info()->offset_element_in_bytes(Coordinates(x, y))] = static_cast<uint8_t>((y * src_shape.x() + x) % 256);
+        }
+    }
+    {
+        uint8_t *ptr = src_f32.buffer();
+        int x, y;
+        for (x = y = 0; true; x++) {
+            if (static_cast<unsigned int>(x) == src_shape.x()) {
+                y++;
+                x = 0;
+                if (src_shape.y() == static_cast<unsigned int>(y)) {
+                    break;
+                }
+            }
+            *(reinterpret_cast<float *>(ptr + src_f32.info()->offset_element_in_bytes(Coordinates(x, y)))) = static_cast<float>((y * src_shape.x() + x) % 256);
         }
     }
 
-    // Save F32 SRC to numpy file
-    const std::string npy1 = "f32_src.npy";
-    save_to_npy(src_f32, npy1, false);
+    // Save src to numpy file
+    const std::string npy2 = "f32_src.npy";
+    save_to_npy(src_f32, npy2, false);
+
+    clock_t begin, end;
+    double  t;
+
+    // Time u8
+    begin = clock();
+    for (int i = 0; i < 10; i++)
+        acl_u8(&dst_u8);
+    end = clock();
+    t = double(end - begin) / CLOCKS_PER_SEC;
+    printf("U8: Elapsed time %.2f S, avg: %.2f mS\n", t, t * 100);
 
     // Time qs8
-    clock_t begin = clock();
+    begin = clock();
     for (int i = 0; i < 10; i++)
         acl_qs8();
-    clock_t end = clock();
-    double t = double(end - begin) / CLOCKS_PER_SEC;
+    end = clock();
+    t = double(end - begin) / CLOCKS_PER_SEC;
     printf("QS8: Elapsed time %.2f S, avg: %.2f mS\n", t, t * 100);
 
     // Time f32
@@ -132,9 +202,34 @@ int main(int argc, const char **argv) {
     t = double(end - begin) / CLOCKS_PER_SEC;
     printf("F32: Elapsed time %.2f S, avg: %.2f mS\n", t, t * 100);
 
-    // Save F32 DST to numpy file
-    const std::string npy2 = "f32_dst.npy";
-    save_to_npy(dst_f32, npy2, false);
+    {
+        uint8_t *buff = dst_u8.buffer();
+        printf("\nU8  out:");
+        for (int i = 0; i < 10; i ++) {
+            printf("\t%d", buff[dst_u8.info()->offset_element_in_bytes(Coordinates(i + 1, 1))]); // Skip border
+        }
+    }
+    {
+
+        uint8_t *buff = dst_f32.buffer();
+        printf("\nF32 out:");
+        for (int i = 0; i < 10; i ++) {
+            printf("\t%.2f", *(reinterpret_cast<float *>(buff + dst_f32.info()->offset_element_in_bytes(Coordinates(i, 0)))));
+        }
+    }
+    {
+        uint8_t *buff = dst_qs8.buffer();
+        printf("\nQS8 out:");
+        for (int i = 0; i < 10; i ++) {
+            printf("\t%d", buff[dst_qs8.info()->offset_element_in_bytes(Coordinates(i, 0))]);
+        }
+    }
+
+    printf("\n");
+
+    // Save dst to numpy file
+    const std::string npy4 = "f32_dst.npy";
+    save_to_npy(dst_f32, npy4, false);
 
     return 1;
 }
